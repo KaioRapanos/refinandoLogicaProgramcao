4fa76a61fc6cb81389ec6dbfaf80b99f16b327fa
KaioRapanos <kaiorapanos@gmail.com>
2023-10-05 23:12:20 -0300

    second commit

---
+++ desafio.md
@@ -0,0 +1,4 @@
+# Classificador de nivel de herói
+
+criar um codigo 
+ter uma varia para armazenar o nome do herói
---
+++ index.js
@@ -0,0 +1,29 @@
+let heroi = {
+    nome: "Gozales",
+    experienciaXP: 9001 
+}
+
+function nivelHeroi(xp){
+    if(xp<=1000){
+        return "Ferro";
+    }else if(xp <= 2000){
+        return "Bronze"
+    }else if(xp <= 5000){
+        return "Prata"
+    }else if(xp <= 7000){
+        return "Ouro"
+    }else if(xp <= 8000){
+        return "Platina"
+    }else if(xp <= 9000){
+        return "Ascendente"
+    }else if(xp <= 10000){
+        return "Imortal"
+    }else if(xp > 10000){
+        return "Radiante"
+    }
+
+
+}
+
+console.log("O Herói de nome " + heroi.nome + 
+    " está no nível de " + nivelHeroi(heroi.experienciaXP));
